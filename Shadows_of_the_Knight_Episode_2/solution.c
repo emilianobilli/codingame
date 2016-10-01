7115baa33955de5764b4de811173ba90a7dcd3de
Your Name <you@example.com>
2016-10-01 22:27:24 +0000

    solution

--- Shadows_of_the_Knight_Episode_2/solution.c
+++ Shadows_of_the_Knight_Episode_2/solution.c
@@ -5,7 +5,7 @@
 
 #define TRUE  0
 #define FALSE 1
-#define MAXSIZE   1000
+#define MAXSIZE   10000 
 #define MOVEMENTS  100
 
 struct point {
@@ -23,15 +23,15 @@ struct point centroid(int mat[][MAXSIZE], int Xmin, int Xmax, int Ymin, int Ymax
 {
     int i,j;
     struct point p;
-    int X = 0;
-    int Y = 0;
-    int T = 0;
+    unsigned long int X = 0;
+    unsigned long int Y = 0;
+    unsigned long int T = 0;
     for ( j = Ymin; j <= Ymax; j++ )
         for ( i = Xmin; i <= Xmax; i++ )
             if (mat[j][i] == TRUE)
             {
-                X += i;
-                Y += j;
+                X += (unsigned long int)i;
+                Y += (unsigned long int)j;
                 T++;
             }
     p.x = (int)(round(X / (double)T));
@@ -64,7 +64,7 @@ int check_movement(struct point *p, struct point p0, int len)
     return 0;
 }
 
-struct point alternative_centroid(struct point C, struct point *p, int len, int mat[][MAXSIZE], int Xmax, int Ymax)
+struct point alternative_centroid(struct point C, struct point *p, int len,int mat[][MAXSIZE], int Xmax, int Ymax)
 {
     struct point nC;
     int x, y;
@@ -129,6 +129,8 @@ struct point alternative_centroid(struct point C, struct point *p, int len, int 
     }
 }
 
+int M[MAXSIZE][MAXSIZE] = {{0}};
+
 /**
  * Auto-generated code below aims at helping you parse
  * the standard input according to the problem statement.
@@ -136,18 +138,19 @@ struct point alternative_centroid(struct point C, struct point *p, int len, int 
 int main()
 {
     struct point mov[MOVEMENTS];
-    int M[MAXSIZE][MAXSIZE] = {0};
     int Xmin;
     int Ymin;
     int Xmax;
     int Ymax;
-    int i,j;
+    int i,j, k;
     struct point p0;
     struct point p1;
     struct point tmp;
     
+    
     int W; // width of the building.
     int H; // height of the building.
+
     scanf("%d%d", &W, &H);
     int N; // maximum number of turns before game over.
     scanf("%d", &N);
@@ -173,6 +176,8 @@ int main()
     while (1) {
         char bombDir[11]; // Current distance to the bomb compared to previous distance (COLDER, WARMER, SAME or UNKNOWN)
         scanf("%s", bombDir);
+        if ((Xmax+1-Xmin) * (Ymax+1-Ymin) <= (1000*1000))
+        {
         if (!strcmp(bombDir,"WARMER"))
         {
             for ( j = Ymin; j <= Ymax; j++ )
@@ -214,7 +219,40 @@ int main()
         p1 =  centroid(M,Xmin,Xmax,Ymin,Ymax);
         if (check_movement(mov,p1,nmov))
             p1 = alternative_centroid(p1,mov,nmov,M,Xmax,Ymax);
-        add_movement(mov,p1, nmov++, MOVEMENTS);    
+        add_movement(mov,p1, nmov++, MOVEMENTS);
+        }
+        else
+        {
+            if (!strcmp(bombDir, "WARMER"))
+            {
+                if (p1.x > p0.x)
+                    Xmin = p0.x + ( p1.x - p0.x );
+                else if (p1.x < p0.x)
+                    Xmax = p0.x - ( p0.x - p1.x );
+                if (p1.y > p0.y)
+                    Ymin = p0.y + (p1.y - p0.y);
+                else if (p1.y < p0.y)
+                    Ymax = p0.y - ( p0.y - p1.y );
+            }
+            if (!strcmp(bombDir, "COLDER"))
+            {
+                if (p1.x > p0.x)
+                    Xmax = p0.x - ( p0.x - p1.x );
+                else if (p1.x < p0.x)
+                    Xmin = p0.x + ( p1.x - p0.x );
+
+                if (p1.y > p0.y)
+                    Ymax = p0.y - ( p0.y - p1.y );
+                else if (p1.y < p0.y)
+                    Ymin = p0.y + (p1.y - p0.y);
+            }
+            p0.x = p1.x;
+            p0.y = p1.y;
+            p1.x = (Xmax-Xmin)/2;
+            p1.y = (Ymax-Ymin)/2;
+            add_movement(mov,p1,nmov++, MOVEMENTS);
+            fprintf(stderr,"%d,%d,%d,%d", Xmin,Xmax,Ymin,Ymax);
+        }
         printf("%d %d\n", p1.x,p1.y);
     }
 
